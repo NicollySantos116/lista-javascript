7087bc06c3fbca417c9a47bb5efa1c10bd2b8886
Nicolly <saantos.nick1606@gmail.com>
2025-04-12 19:17:03 -0300

    feat(if-else): adiciona classificação de clima por temperatura

---
+++ If_Else/19_climadodia.js
@@ -0,0 +1,9 @@
+let temp = 23;
+
+if(temp < 15){
+    console.log(" O Clima está Frio ❄️");
+}  else if (temp <= 25){
+    console.log("O Clima está Agradável 🌤️");
+} else {
+    console.log("O Clima está Quente 🌡️")
+}
