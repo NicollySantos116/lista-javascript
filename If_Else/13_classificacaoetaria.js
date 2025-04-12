e0f0a8d8770abc59201f22351572175d0e52f858
Nicolly <saantos.nick1606@gmail.com>
2025-04-12 18:27:51 -0300

    feat(if-else): adiciona classificação etária por idade

---
+++ If_Else/13_classificacaoetaria.js
@@ -0,0 +1,11 @@
+let idade = 45;
+
+if (idade >= 0 && idade <= 12 ){
+    console.log("Criança 👶");
+} else if (idade <= 17){
+    console.log ("Adolecente 👩‍🦰");
+} else if (idade <= 59){
+    console.log("Adulto 👨‍🦰");
+} else {
+    console.log ("Idoso 🧓")
+}
