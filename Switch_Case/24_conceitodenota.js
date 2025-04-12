8f5731d6fd6d2beb30800724416a80770d25ed7f
Nicolly <saantos.nick1606@gmail.com>
2025-04-12 20:13:53 -0300

    feat(switch-case): adiciona atribuição de conceito por nota

---
+++ Switch_Case/24_conceitodenota.js
@@ -0,0 +1,19 @@
+const nota = 7;
+
+switch (true) {
+    case nota >= 9:
+        console.log("A");
+        break;
+    case nota >= 7:
+        console.log("B");
+        break;
+    case nota >= 5:
+        console.log("C");
+        break;
+    case nota >= 3:
+        console.log("D");
+        break;
+    default:
+        console.log("E");
+
+}
