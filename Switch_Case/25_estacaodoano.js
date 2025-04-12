8870d91e374846ca4089a7d1a754869f1a69ffde
Nicolly <saantos.nick1606@gmail.com>
2025-04-12 20:36:16 -0300

    feat(switch-case): adiciona exibição da estação do ano

---
+++ Switch_Case/25_estacaodoano.js
@@ -0,0 +1,18 @@
+const estacao = 2;
+
+switch (estacao) {
+    case 1:
+        console.log("Verão");
+        break;
+    case 2:
+        console.log("Outono");
+        break;
+    case 3:
+        console.log("Inverno");
+        break;
+    case 4:
+        console.log("Primavera");
+        break;
+    default:
+        console.log("Código inválido");
+}
